ea8106fe2dc23fb1e7e6b9d549993b96f78674e3
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-03 15:36:05 +0600

    header

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,10 +1,29 @@
-
-
+import { TiThMenu } from "react-icons/ti";
+import { IoSearchSharp } from "react-icons/io5";
 const Header = () => {
-    return (
-        <div>
-            header
-        </div>
+return (
+<div className="bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-b border-slate-200/50 dark:border-slate-700/50 px-6 py-4">
+<div className="flex items-center justify-between">
+{/*left section  */}
+<div className="flex items-center space-x-4">
+<button className="p-2 rounded-lg text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors">
+<TiThMenu className="size-5"/>
+</button>
+<div className="hidden md:block">
+<h1 className="text-2xl font-black text-slate-800 dark:text-white ">Dashboard</h1>    
+<p className="dark:text-slate-400">Welcome back, Tanjila! here's what's happening today</p>
+</div>
+</div>
+{/* center */}
+<div className="flex-1 max-w-md mx-8">
+<div className="relative">
+<IoSearchSharp className="size-4 absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400"/>
+<input type="text" placeholder="Search Anything" className="w-full pl-10 pr-4 py-2.5 bg-slate-100 dark:bg-slate-800 border border-slate-200 dark:border-slate-700 rounded-xl text-slate-800 dark:text-white placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"/>
+</div>    
+</div>    
+
+</div>
+</div> 
     );
 };
 
